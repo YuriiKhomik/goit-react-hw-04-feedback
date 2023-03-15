d0d3a253114f2bc930dd934bdd3959e3868c4993
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-03-15 11:16:30 +0200

    ...

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState, Component } from 'react';
 import { percentageCounter } from 'utils';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Section from './Section';
 import Notification from './Notification';
 
-function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+// // via useReducer
 
-  const options = ['good', 'neutral', 'bad'];
+const feedbackState = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+function feedbackReducer(state, action) {
+  switch (action.type) {
+    case 'good':
+      return { ...state, good: state.good + action.payload };
+    case 'neutral':
+      return { ...state, neutral: state.neutral + action.payload };
+    case 'bad':
+      return { ...state, bad: state.bad + action.payload };
+
+    default:
+      throw new Error(`Unsupported action type ${action.type}`);
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(feedbackReducer, feedbackState);
+
+  const options = Object.keys(state);
+  const { good, neutral, bad } = state;
   const total = good + neutral + bad;
   const positivePercentage = percentageCounter(total, good);
 
@@ -19,15 +40,15 @@ function App() {
 
     switch (name) {
       case 'good':
-        setGood(prevState => prevState + 1);
+        dispatch({ type: 'good', payload: 1 });
         break;
 
       case 'neutral':
-        setNeutral(prevState => prevState + 1);
+        dispatch({ type: 'neutral', payload: 1 });
         break;
 
       case 'bad':
-        setBad(prevState => prevState + 1);
+        dispatch({ type: 'bad', payload: 1 });
         break;
 
       default:
@@ -64,6 +85,70 @@ function App() {
     </div>
   );
 }
+
+// // via useState
+
+// function App() {
+//   const [good, setGood] = useState(0);
+//   const [neutral, setNeutral] = useState(0);
+//   const [bad, setBad] = useState(0);
+
+//   const options = ['good', 'neutral', 'bad'];
+//   const total = good + neutral + bad;
+//   const positivePercentage = percentageCounter(total, good);
+
+//   const handleClick = e => {
+//     const { name } = e.currentTarget;
+
+//     switch (name) {
+//       case 'good':
+//         setGood(prevState => prevState + 1);
+//         break;
+
+//       case 'neutral':
+//         setNeutral(prevState => prevState + 1);
+//         break;
+
+//       case 'bad':
+//         setBad(prevState => prevState + 1);
+//         break;
+
+//       default:
+//         return;
+//     }
+//   };
+
+//   return (
+//     <div
+//       style={{
+//         paddingTop: '100px',
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'flex-start',
+//         fontSize: 20,
+//         color: '#010101',
+//       }}
+//     >
+//       <Section title="Please leave feedback">
+//         <FeedbackOptions options={options} onLeaveFeedback={handleClick} />
+//         {total > 0 ? (
+//           <Statistics
+//             good={good}
+//             neutral={neutral}
+//             bad={bad}
+//             total={total}
+//             positivePercentage={positivePercentage}
+//           />
+//         ) : (
+//           <Notification message="There is no feedback" />
+//         )}
+//       </Section>
+//     </div>
+//   );
+// }
+
+// // via class-component
 
 // class App extends Component {
 //   state = {
